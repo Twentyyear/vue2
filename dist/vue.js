9d5b3414fbec27468b9cbdceb6b7f694c35862af
Twentyyear <827258516@qq.com>
2023-01-14 13:07:51 +0800

    数据的初始化

--- dist/vue.js
+++ dist/vue.js
@@ -13,7 +13,6 @@
     function initData(vm) {
       var data = vm.$options.data; //data可能是函数或者对象
       data = typeof data === "function" ? data.call(vm) : data;
-      console.log(data);
     }
 
     function initMixin(Vue) {
