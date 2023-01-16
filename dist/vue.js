702f484e9c2f728d279398d94c5c16253540813b
Twentyyear <827258516@qq.com>
2023-01-16 19:41:12 +0800

    模板编译

--- dist/vue.js
+++ dist/vue.js
@@ -196,6 +196,12 @@
     }
   }
 
+  function compileToFunction(template) {
+    // 1.就是将template 转化成ast语法树
+    //2.生成render方法 (render方法执行后返回的结果就是 虚拟DOM)
+    console.log(template);
+  }
+
   function initMixin(Vue) {
     //就是给Vue增加init方法
     Vue.prototype._init = function (options) {
@@ -207,6 +213,38 @@
       vm.$options = options; //将用户的选项挂载到实例上
       //初始化状态
       initState(vm);
+      if (options.el) {
+        vm.$mount(options.el); //实现数据的挂载
+      }
+    };
+
+    Vue.prototype.$mount = function (el) {
+      var vm = this;
+      el = document.querySelector(el);
+      var ops = vm.$options;
+      if (!ops.render) {
+        // 先进行查找有没有render函数
+        var template; //没有render函数看一下是否写了template  没写template 采用外部的template
+        if (!ops.template && el) {
+          //没有写模板 但是写了el
+          template = el.outerHTML; //在火狐下可能不兼容
+        } else {
+          if (el) {
+            template = ops.template;
+          }
+        }
+        //写了template就用写了的template
+        if (template) {
+          //这里需要对模板进行编译
+          var render = compileToFunction(template);
+          ops.render = render; //jsx 最终会被编译成为h('xxx')
+        }
+      }
+
+      ops.render; //最终就可以获取render方法
+
+      //script 标签引用的vue.global.js 这个编译过程是在浏览器运行的
+      // runtime是不包含模板编译的,整个编译是打包的时候通过loader来转义.vue文件的, 用runtime的时候不能使用template
     };
   }
 
---
+++ src/compiler/index.js
@@ -0,0 +1,5 @@
+export function compileToFunction(template) {
+    // 1.就是将template 转化成ast语法树
+    //2.生成render方法 (render方法执行后返回的结果就是 虚拟DOM)
+    console.log(template)
+}
